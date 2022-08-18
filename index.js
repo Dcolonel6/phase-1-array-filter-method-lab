bd31a325bedcd496892261edcee54eabf46b82c6
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-18 21:59:23 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+const findMatching = (listOfDriverNames,q) => listOfDriverNames.filter(driver => q.toLowerCase() === driver.toLowerCase())
+
+function fuzzyMatch(listOfDriverNames,query){
+    return listOfDriverNames.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()))
+}
+const matchName = function(listOfObjDrivers, q){
+    return listOfObjDrivers.filter(({name,hometown}) => name.toLowerCase() === q.toLowerCase())
+}
